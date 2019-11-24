4d76694120a76617bc5925f687c4e5e798d6c1da
Neonidian <neonidian@gmail.com>
2019-11-24 12:56:00 +0100

    Split into methods

--- C#/BinaryGap/BinaryGap/Program.cs
+++ C#/BinaryGap/BinaryGap/Program.cs
@@ -8,47 +8,57 @@ namespace BinaryGap
         static void Main(string[] args)
         {
             Solution solution = new Solution();
-            Console.WriteLine("Binary gap for the number is {0}", solution.solution(1033)); //145 - 10010001; 137 - 10001001; 1033 - 10000001001;
+            Console.WriteLine("Binary gap for the number is {0}", solution.CalculateBinaryGap(1033)); //145 - 10010001; 137 - 10001001; 1033 - 10000001001;
         }
     }
 
     class Solution
     {
-        public int solution(int N)
+        public int CalculateBinaryGap(int decimalNumber)
         {
-            string binary = Convert.ToString(N, 2);
+            string binary = Convert.ToString(decimalNumber, 2);
             int lengthOfBinaryNumber = binary.Length;
             char[] binaryAsArray = binary.ToCharArray();
-            int tempBinaryGap = 0;
-            int binaryGap = 0;
-            List<int> occurencesOfBinaryNumber = new List<int>();
+            List<int> occurencesOfBinaryNumber;
+            int binaryGap;
 
-            // Find the position of the occurences of '1' in the binary number and store the positions in a list
-            for (int j = 0; j < lengthOfBinaryNumber; j++)
-            {
-                if(binaryAsArray[j] == '1')
-                {
-                    occurencesOfBinaryNumber.Add(j);
-                }
-            }
-
+            occurencesOfBinaryNumber = FindOccurencesOfOneInBinaryNumber(lengthOfBinaryNumber, binaryAsArray);
             int lengthOccurenceOfBinaryNumber = occurencesOfBinaryNumber.Count;
+            binaryGap = FindBinaryGap(occurencesOfBinaryNumber, lengthOccurenceOfBinaryNumber);
+            return binaryGap;
+        }
 
-            // Find the largest binary Gap
+        private int FindBinaryGap(List<int> occurencesOfBinaryNumber, int lengthOccurenceOfBinaryNumber)
+        {
+            int tempBinaryGap = 0;
+            int binaryGapNumber = 0;
             if (lengthOccurenceOfBinaryNumber > 1)
             {
                 int temp = 0;
                 for (int i = 1; i < lengthOccurenceOfBinaryNumber; i++)
                 {
-                    temp = occurencesOfBinaryNumber[i] - occurencesOfBinaryNumber[i-1] - 1;
-                    if(temp > tempBinaryGap)
+                    temp = occurencesOfBinaryNumber[i] - occurencesOfBinaryNumber[i - 1] - 1;
+                    if (temp > tempBinaryGap)
                     {
-                        binaryGap = temp;
+                        binaryGapNumber = temp;
                         tempBinaryGap = temp;
                     }
                 }
             }
-            return binaryGap;
+            return binaryGapNumber;
+        }
+
+        private List<int> FindOccurencesOfOneInBinaryNumber(int lengthOfBinaryNumber, char[] binaryAsArray)
+        {
+            List<int> occurencesOfBinaryNumber = new List<int>();
+            for (int j = 0; j < lengthOfBinaryNumber; j++)
+            {
+                if (binaryAsArray[j] == '1')
+                {
+                    occurencesOfBinaryNumber.Add(j);
+                }
+            }
+            return occurencesOfBinaryNumber;
         }
     }
 }
