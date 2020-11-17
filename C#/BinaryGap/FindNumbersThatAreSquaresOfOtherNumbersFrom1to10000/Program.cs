ff0d229ca0346a5f997343ef59857b0175743833
Keerthi Shankar Subramanian <keerthi.subramanian@cepheid.com>
2020-11-17 22:41:27 +0100

    Add FindNumbersThatAreSquaresFrom1To10000

---
+++ C#/BinaryGap/FindNumbersThatAreSquaresOfOtherNumbersFrom1to10000/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace FindNumbersThatAreSquaresOfOtherNumbersFrom1to10000
+{
+    class Program
+    {
+        private static short bb = -1280;
+        
+        static void Main()
+        {
+            foreach (var findNumbersThatAreSquaresOfOtherNumber in FindNumbersThatAreSquaresOfOtherNumbers())
+            {
+                Console.WriteLine(findNumbersThatAreSquaresOfOtherNumber);
+            }
+        }
+
+        static int[] FindNumbersThatAreSquaresOfOtherNumbers()
+        {
+            int[] arr = new int[100];
+            int j = 0;
+            
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (i * i <= 10000)
+                {
+                    arr[j] = i * i;
+                    j++;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
